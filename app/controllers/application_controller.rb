a50086b9d2682603852ff18c8e27bab28a6664d3
Allie LeFever <alefever39@gmail.com>
2022-07-05 11:59:48 -0600

    commit tree ed5b49453dc4a629416688dc4b54819b16a086c1

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,7 +1,30 @@
 class ApplicationController < Sinatra::Base
+  set :default_content_type, "application/json"
 
-  get '/' do
+  get "/" do
     { message: "Hello world" }.to_json
   end
 
+  get "/games" do
+    games = Game.all.order(:title).limit(10)
+    games.to_json
+  end
+
+  get "/games/:id" do
+    id = params[:id]
+    game = Game.find(id)
+    game.to_json(
+      only: %i[id title genre price],
+      include: {
+        reviews: {
+          only: %i[comment score],
+          include: {
+            user: {
+              only: [:name]
+            }
+          }
+        }
+      }
+    )
+  end
 end
